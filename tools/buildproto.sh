cec932340b84a33cb583f7531c8b0e1c02289e14
TakashiKannan <wingrakhi@live.jp>
2019-07-13 15:30:38 +0900

    build rpc sample

--- tools/buildproto.sh
+++ tools/buildproto.sh
@@ -1,8 +1,20 @@
-# C#
-protoc -I ./protocolbuffer \
-    --csharp_out unity/Assets/M3Online/Protoc \
-    --grpc_out unity/Assets/M3Online/Protoc ./protocolbuffer/*.proto \
+#sh
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+SERVER_SRC=$REPO_ROOT/server/src/m3online/
+SERVER_OUTPUT_DIR=$SERVER_SRC/rpc/
+CLIENT_OUTPUT_DIR=$REPO_ROOT/unityclient/Assets/M3Online/Grpc/
+GRPC_SOURCE=$REPO_ROOT/protocolbuffer
+
+mkdir -p $CLIENT_OUTPUT_DIR
+mkdir -p $SERVER_OUTPUT_DIR
+rm -rf $CLIENT_OUTPUT_DIR/*.cs
+rm -rf $SERVER_OUTPUT_DIR/*.go
+
+protoc --proto_path=$GRPC_SOURCE \
+    --csharp_out=$CLIENT_OUTPUT_DIR/ \
+    --go_out=$SERVER_OUTPUT_DIR \
+    --grpc_out=$CLIENT_OUTPUT_DIR/ \
+    -I=$GRPC_SOURCE/ \
+    $GRPC_SOURCE/*/*.proto  \
     --plugin=protoc-gen-grpc=/usr/local/bin/grpc_csharp_plugin
- 
-# Go
-protoc -I ./protocolbuffer --go_out=plugins=grpc:./server/src/m3online ./protocolbuffer/*.proto
