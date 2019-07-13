74af95b7e8f4becb40eae3c32ea4e2c2799267af
TakashiKannan <wingrakhi@live.jp>
2019-07-14 01:08:09 +0900

    implement connect and attack

--- server/src/m3online/controller/battleController.go
+++ server/src/m3online/controller/battleController.go
@@ -2,22 +2,63 @@ package controller
 
 import (
 	"context"
+	"io"
 	"log"
 	"m3online/rpc"
+	"m3online/service"
 )
 
 type BattleController struct {
+	enemyService *service.EnemyService
 }
 
 var entryCount = uint32(0)
 
 func (BattleController) EnterBattle(context.Context, *rpc.EnterRequest) (*rpc.EnterResponse, error) {
 	entryCount = entryCount + 1
-	var enter = &rpc.Enter{EnterId:entryCount}
+	var enter = &rpc.Enter{EnterId: entryCount}
 
 	log.Printf("%d", entryCount)
 
-	return &rpc.EnterResponse{Enter:enter}, nil
+	return &rpc.EnterResponse{Enter: enter}, nil
+}
+
+func (controller BattleController) Connect(request *rpc.ConnectionRequest, stream rpc.BattleService_ConnectServer) error {
+	//ToDO: ConnectionRequestCheck
+
+	for {
+		if false { // ToDo CheckConnectionBreak
+			break
+		}
+		if false { //ToDo CheckDiff Also Check Dirty
+			continue
+		}
+		enemy, _ := controller.enemyService.GetEnemy()
+		var situation = &rpc.EnemySituation{Enemy: enemy}
+		if err := stream.Send(situation); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (controller BattleController) Attack(stream rpc.BattleService_AttackServer) error {
+	for {
+		var attackRequest, err = stream.Recv()
+		if err == io.EOF {
+			var summery = rpc.SessionSummary{}
+			return stream.SendAndClose(&summery)
+		}
+		if err != nil {
+			return err
+		}
+
+		//ToDo: AttackRequestCheck
+		err = controller.enemyService.AttackEnemy(attackRequest.Attack)
+		if err != nil {
+			log.Printf("Error:%s", err)
+		}
+	}
 }
 
 var _ rpc.BattleServiceServer = (*BattleController)(nil)
---
+++ server/src/m3online/service/enemyService.go
@@ -0,0 +1,46 @@
+package service
+
+import "m3online/rpc"
+
+var currentEnemy = rpc.Enemy{}
+
+const (
+	masterId   = 0
+	enemyMaxHp = 10
+	enemyName  = "Slime"
+)
+
+type EnemyService struct {
+}
+
+func (EnemyService) Initialize() error {
+	currentEnemy.InstantId = uint32(0)
+	currentEnemy.MasterId = masterId
+	currentEnemy.Name = enemyName
+	currentEnemy.Hp = enemyMaxHp
+	return nil
+}
+
+func (EnemyService) GetEnemy() (*rpc.Enemy, error) {
+	return &currentEnemy, nil
+}
+
+func createEnemy() (rpc.Enemy, error) {
+	var enemy = rpc.Enemy{}
+	enemy.InstantId = currentEnemy.InstantId + 1
+	enemy.MasterId = masterId
+	enemy.Hp = enemyMaxHp
+	enemy.Name = enemyName
+	return enemy, nil
+}
+
+func (EnemyService) AttackEnemy(attack *rpc.Attack) error {
+
+	currentEnemy.Hp = currentEnemy.Hp - 1
+
+	if currentEnemy.Hp <= 0 {
+		currentEnemy, _ = createEnemy()
+	}
+
+	return nil
+}
